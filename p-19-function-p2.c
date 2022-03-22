d392aa507aa9391d12e198831f2d2d0bc8fa7c8b
marzia2936 <67289417+marzia2936@users.noreply.github.com>
2022-03-22 23:54:16 +0600

    Add files via upload

---
+++ p-19-function-p2.c
@@ -0,0 +1,12 @@
+/*Write a function that simply prints a message "Hello!! I am from inside
+of a function". call this function from the main function */
+#include<stdio.h>
+void print_message(){
+printf("Hello!! I am from inside of a function\n");
+}
+
+
+int main(){
+print_message();
+return 0;
+}
